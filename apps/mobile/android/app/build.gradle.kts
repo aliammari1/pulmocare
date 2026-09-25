94fe10621cb05996c3c38429897af82c0d7868ae
Ali Ammari <ammari.ali.0001@gmail.com>
2026-09-25 17:10:29 +0100

    chore(mobile): target Flutter 3.47 supported Android floor

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         applicationId = "com.pulmocare.app"
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
